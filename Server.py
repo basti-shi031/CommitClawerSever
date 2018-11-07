e6c8842eed7f216d124c09147ffe213172460a7c
calvinhkf <cavinkfh@gmail.com>
2018-11-07 09:54:57 +0800

    server

--- Server.py
+++ Server.py
@@ -158,16 +158,33 @@ class PostHandler(BaseHTTPRequestHandler):
         if path.startswith('/fetchFile'):
             # 向minner请求指定文件和diff link
             fetchFile(self, form)
-        elif path.startswith('/clearCommitRecord'):
-            #     清空数据库缓存，清除
-            clearCommitTable()
-            clearOutputInServer(self)
         elif path.startswith('/fetchMeta'):
             # 根据本地是否有缓存请求数据
             # 如果有缓存，直接向minner请求meta数据
             # 如果没有，向github爬去文件和meta信息，交给minner存储。
             fetchMeta(self, form)
+        else:
+            self.send_response(200)
+            DUMMY_RESPONSE = "dummy request"
+            self.send_header("Content-type", "text/html")
+            self.send_header("Content-length", len(DUMMY_RESPONSE))
+            self.end_headers()
+            self.wfile.write(DUMMY_RESPONSE.encode('utf-8'))
 
+    def do_GET(self):
+        path = self.path
+        if path.startswith('/clearCommitRecord'):
+            #     清空数据库缓存，清除
+            print("clear record")
+            clearCommitTable()
+            clearOutputInServer(self)
+        else:
+            self.send_response(200)
+            DUMMY_RESPONSE = "dummy request"
+            self.send_header("Content-type", "text/html")
+            self.send_header("Content-length", len(DUMMY_RESPONSE))
+            self.end_headers()
+            self.wfile.write(DUMMY_RESPONSE.encode('utf-8'))
 
 
 def StartServer():
